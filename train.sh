c6633d5bcca69bfd730b3f55f45dca59b6301e7a
chenjiang <chenjiang@bigo.sg>
2020-03-31 17:55:32 +0800

    mod

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 # @File Name : train.sh
 # @Purpose :
 # @Creation Date : 2020-03-21 15:12:49
-# @Last Modified : 2020-03-31 17:44:34
+# @Last Modified : 2020-03-31 17:55:10
 # @Created By :  chenjiang
 # @Modified By : chenjiang
 
@@ -50,6 +50,11 @@ valid_B_dir="data/vcc2018_training.speakers/VCC2TM1/"
 # valid_A_dir="data/vctk_sub/p270/"
 # valid_B_dir="data/vctk_sub/p256/"
 
+save_interval=100
+if [ ${cache_dir} == *"all_"* ];then
+    save_interval=10
+fi
+
 python preprocess_training.py --train_A_dir ${train_A_dir} \
                               --train_B_dir ${train_B_dir} \
                               --cache_folder ${cache_dir} 
@@ -64,7 +69,8 @@ python train_cyclegan_vc2.py --logf0s_normalization   ${cache_dir}/logf0s_normal
                              --output_A_dir           ${cache_dir}/converted_sound/VCC2SF3/ \
                              --validation_B_dir       ${valid_B_dir} \
                              --output_B_dir           ${cache_dir}/converted_sound/VCC2TM1/ \
-                             --log_dir                ${cache_dir}/logs
+                             --log_dir                ${cache_dir}/logs \
+                             --save_interval ${save_interval}
                              # --resume_training_at     ${cache_dir}/model_checkpoint/_CycleGAN_CheckPoint \
 
 
